e4cf870fa7f03d59d7cf15c9abf2f1862fab572e
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-10-25 23:20:47 +0600

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path'; //for production;
+
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
@@ -9,17 +11,32 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); //For production
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-  })
-);
+//middleware
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+    })
+  );
+}
 
-app.use(express.json()); //middleware
+app.use(express.json());
 app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
+
+// For Productions only
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
+
+// For Productions only end
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,13 @@ import NoteDetailPage from './pages/NoteDetailPage';
 
 const App = () => {
   return (
-    <div className="relative h-full w-full">
-      <div
-        className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24"
-        style={{
-          background:
-            'radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)',
-        }}
-      ></div>
+    <div
+      className="h-full w-full "
+      style={{
+        background:
+          'radial-gradient(125% 125% at 50% 10%, #000 40%, #63e 100%)',
+      }}
+    >
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
